c8b62f165a13f1554d0204abffb136e057e4a7e1
Michael Buhler <michael@michaelbuhler.com>
2023-06-21 22:00:40 +0700

    expand the properties of `Boolean` and its prototype

--- lib/builtin_objects.cpp
+++ lib/builtin_objects.cpp
@@ -18,6 +18,16 @@ shared_ptr<Value> _Boolean (shared_ptr<Scope> callingScope, vector<shared_ptr<Va
   return ToBoolean(arguments[0]);
 }
 
+shared_ptr<Value> _Boolean_toString (shared_ptr<Scope> callingScope, vector<shared_ptr<Value> > arguments) {
+  // TODO: needs the context of a `this` arg
+  throw new NotImplementedException("Boolean.prototype.toString()");
+}
+
+shared_ptr<Value> _Boolean_valueOf (shared_ptr<Scope> callingScope, vector<shared_ptr<Value> > arguments) {
+  // TODO: needs the context of a `this` arg
+  throw new NotImplementedException("Boolean.prototype.valueOf()");
+}
+
 void init_builtin_objects (shared_ptr<Scope> globalScope) {
   shared_ptr<Object> FunctionPrototype = make_shared<Object>(nullptr);
   shared_ptr<Object> Function = make_shared<Object>(FunctionPrototype, _Boolean);
@@ -29,5 +39,10 @@ void init_builtin_objects (shared_ptr<Scope> globalScope) {
 
   shared_ptr<Object> BooleanPrototype = make_shared<Object>(nullptr);
   shared_ptr<Object> Boolean = make_shared<Object>(BooleanPrototype, _Boolean);
+  Boolean->__Put__("prototype", BooleanPrototype);
+  Boolean->__Put__("length", make_shared<Number>(1));
+  BooleanPrototype->__Put__("constructor", Boolean);
+  BooleanPrototype->__Put__("toString", make_shared<Object>(nullptr, _Boolean_toString));
+  BooleanPrototype->__Put__("valueOf", make_shared<Object>(nullptr, _Boolean_valueOf));
   globalScope->set("Boolean", Boolean);
 }
