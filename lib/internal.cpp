ce9decff227acfb6a14f85ceff5e79a397464808
Michael Buhler <michael@michaelbuhler.com>
2023-06-23 23:29:42 +0700

    improve var naming and error messaging

--- lib/internal.cpp
+++ lib/internal.cpp
@@ -8,15 +8,15 @@
 
 using namespace std;
 
-shared_ptr<Value> _call (shared_ptr<Value> value, shared_ptr<Scope> scope, vector<shared_ptr<Value> > params) {
-  if (value->type != OBJECT_VALUE_TYPE) {
+shared_ptr<Value> _call (shared_ptr<Value> callee, shared_ptr<Scope> scope, vector<shared_ptr<Value> > params) {
+  if (callee->type != OBJECT_VALUE_TYPE) {
     throw TypeError("callee is not an object");
   }
-  shared_ptr<Object> obj = static_pointer_cast<Object>(value);
+  shared_ptr<Object> obj = static_pointer_cast<Object>(callee);
   if (obj->__Call__ == nullptr) {
-    throw TypeError("object has no call signature");
+    throw TypeError("object is not a function");
   }
   shared_ptr<Scope> functionScope = make_shared<Scope>(scope);
-  functionScope->set("this", value);
+  functionScope->set("this", callee);
   return obj->__Call__(obj, functionScope, params);
 }
