bd83253b922b7a2cf9fffb46e2bf5761c7c12bb1
Michael Buhler <michael@michaelbuhler.com>
2023-06-24 13:31:25 +0700

    `[[Class]]` should be set on construction

--- lib/builtin_objects.cpp
+++ lib/builtin_objects.cpp
@@ -26,6 +26,7 @@ shared_ptr<Value> _Boolean__Call__ (shared_ptr<Value> _this, shared_ptr<Scope> s
 
 shared_ptr<Object> _Boolean__Construct__ (shared_ptr<Scope> scope, vector<shared_ptr<Value> > arguments) {
   shared_ptr<Object> _this = static_pointer_cast<Object>(scope->*"this");
+  _this->__Class__ = "Boolean"; // TODO: enum this
   if (arguments.size() == 0) _this->__Value__ = make_shared<Boolean>(false);
   else _this->__Value__ = ToBoolean(arguments[0]);
   return _this;
@@ -59,7 +60,6 @@ shared_ptr<Value> _Boolean_prototype_valueOf (shared_ptr<Value> _this, shared_pt
 void init_builtin_objects (shared_ptr<Scope> globalScope) {
   shared_ptr<Object> FunctionPrototype = make_shared<Object>();
   shared_ptr<Object> Function = make_shared<Object>(FunctionPrototype, _Function__Call__, _Function__Construct__);
-  Function->__Class__ = "Function"; // TODO: enum this
   Function->__Put__("prototype", FunctionPrototype);
   Function->__Put__("length", make_shared<Number>(1));
   FunctionPrototype->__Put__("constructor", Function);
@@ -69,7 +69,6 @@ void init_builtin_objects (shared_ptr<Scope> globalScope) {
   // TODO: Boolean.prototype should be 'a Boolean object whose value is false'.
   shared_ptr<Object> BooleanPrototype = make_shared<Object>();
   shared_ptr<Object> Boolean = make_shared<Object>(BooleanPrototype, _Boolean__Call__, _Boolean__Construct__);
-  Boolean->__Class__ = "Boolean"; // TODO: enum this
   Boolean->__Put__("prototype", BooleanPrototype);
   Boolean->__Put__("length", make_shared<Number>(1));
   BooleanPrototype->__Put__("constructor", Boolean);
