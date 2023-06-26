b018d5d9d56db52383443ce834b7ae38712be95d
Michael Buhler <michael@michaelbuhler.com>
2023-06-26 16:31:03 +0700

    stub global `eval()` function

--- src/global_scope.cpp
+++ src/global_scope.cpp
@@ -1,10 +1,15 @@
 #include "global_scope.h"
 
 #include "builtin_objects.h"
+#include "exception.h"
 #include "host_objects.h"
 #include "type_conversion.h"
 
 using namespace std;
+
+shared_ptr<Value> _eval__Call__ (shared_ptr<Value> _this, shared_ptr<Scope> scope, vector<shared_ptr<Value> > arguments) {
+  throw NotImplementedException("Cannot use global `eval()` to execute arbitrary code at runtime.");
+}
 
 shared_ptr<Value> _isNaN__Call__ (shared_ptr<Value> _this, shared_ptr<Scope> scope, vector<shared_ptr<Value> > arguments) {
   // TODO: reimplement this after we have `arguments` and names/formal parameters
