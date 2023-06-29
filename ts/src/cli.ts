b5b436793aaa6d404a92a309ea3aee25bf4602c4
Michael Buhler <michael@michaelbuhler.com>
2023-06-29 20:00:36 +0700

    add `argparse` to razuberi cli

--- ts/src/cli.ts
+++ ts/src/cli.ts
@@ -1,1 +1,27 @@
-console.log("razuberi")
+
+import * as path from 'node:path'
+
+import { ArgumentParser } from 'argparse'
+
+const argParser = new ArgumentParser({
+  description: 'Razuberi: a JavaScript -> TypeScript transpiler.',
+  prog: 'razuberi',
+})
+
+argParser.add_argument('inputFilename', {
+  help: 'JavaScript/TypeScript file to transpile',
+  metavar: '<input>',
+  type: (arg: string) => path.join(process.cwd(), arg),
+})
+
+argParser.add_argument('-o', '--out', {
+  default: path.join(process.cwd(), 'a.cpp'),
+  dest: 'outputFilename',
+  help: 'write output C++ to <file>. defaults to `a.cpp`',
+  metavar: '<file>',
+  type: (arg: string) => path.join(process.cwd(), arg),
+})
+
+const args = argParser.parse_args()
+
+console.log(args)
