db4af42ca1a0785ff574bf07e54b11bb860ac7bb
Michael Buhler <michael@michaelbuhler.com>
2023-06-29 22:00:32 +0700

    transpile to stringified babel ast

--- ts/src/lib/transpile.ts
+++ ts/src/lib/transpile.ts
@@ -2,10 +2,18 @@
 const { default: typescript } = await import('typescript')
 const { transpile: ts2js, ScriptTarget } = typescript
 
+const { parse: js2ast } = await import('@babel/parser')
+
 export const transpile = (typescriptSource: string) : string => {
   const javascriptSource = ts2js(typescriptSource, {
     target: ScriptTarget.ES3,
   })
 
-  return javascriptSource
+  const ast = js2ast(javascriptSource, {
+    annexB: false,
+    errorRecovery: true,
+    sourceType: 'script',
+  })
+
+  return JSON.stringify(ast, null, 2)
 }
