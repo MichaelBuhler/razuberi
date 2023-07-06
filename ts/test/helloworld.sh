ad1851b2de85f414cbd22bc6e1fe555af49dafb0
Michael Buhler <michael@michaelbuhler.com>
2023-07-07 01:21:21 +0700

    run the `indent` program on the test code
    it is not perfect, but at least the leading spacing is readable

--- ts/test/helloworld.sh
+++ ts/test/helloworld.sh
@@ -2,6 +2,7 @@
 set -e
 
 bin/razuberi.js test/helloworld.ts -o test/helloworld.cpp
+cd test && indent helloworld.cpp && cd ..
 g++ -c -I ../cpp/out/include test/helloworld.cpp -o test/helloworld.o
 g++ -L ../cpp/out/lib -l razuberi test/helloworld.o -o test/helloworld
 test/helloworld
