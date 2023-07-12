3a413979e240818fe17f77284c974b8849fbd8d3
Michael Buhler <michael@michaelbuhler.com>
2023-07-12 16:24:15 +0700

    improve `console.log`, including printing negative zeroes

--- cpp/src/host_objects.cpp
+++ cpp/src/host_objects.cpp
@@ -14,17 +14,36 @@ shared_ptr<Value> Console_log (shared_ptr<Scope>, shared_ptr<Value>, vector<shar
   for ( int i = 0 ; i < arguments.size() ; i++ ) {
     if (i != 0) cout << " ";
     shared_ptr<Value> arg = arguments[i];
-    shared_ptr<String> str = nullptr;
-    if (arg->type == OBJECT_VALUE_TYPE) {
-      shared_ptr<Object> obj = static_pointer_cast<Object>(arg);
-      if (obj->__HasProperty__("toString")) {
-        str = ToString((obj->*"toString").call());
-      }
+    switch (arg->type) {
+      case UNDEFINED_VALUE_TYPE:
+        cout << "undefined";
+        break;
+      case NULL_VALUE_TYPE:
+        cout << "null";
+        break;
+      case BOOLEAN_VALUE_TYPE:
+        cout << ToString(static_pointer_cast<Boolean>(arg))->value;
+        break;
+      case NUMBER_VALUE_TYPE: {
+        shared_ptr<Number> num = static_pointer_cast<Number>(arg);
+        if (!num->isNaN && !num->isInfinity && num->value == 0 && num->isNegative) {
+          cout << "-0";
+        } else {
+          cout << ToString(static_pointer_cast<Number>(arg))->value;
+        }
+      } break;
+      case STRING_VALUE_TYPE:
+        cout << static_pointer_cast<String>(arg)->value;
+        break;
+      case OBJECT_VALUE_TYPE: {
+        shared_ptr<Object> obj = static_pointer_cast<Object>(arg);
+        if (obj->__HasProperty__("toString")) {
+          cout << ToString((obj->*"toString").call())->value;
+        } else {
+          cout << ToString(obj)->value;
+        }
+      } break;
     }
-    if (str.get() == nullptr) {
-      str = ToString(arg);
-    }
-    cout << str->value;
   }
   cout << endl;
   return make_shared<Undefined>();
