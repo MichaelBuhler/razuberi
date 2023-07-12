00bfccd2d9f32619b69dbf9a8ffc4d5c7b82d345
Michael Buhler <michael@michaelbuhler.com>
2023-07-12 16:35:26 +0700

    add `producedReference` codegen utility function

--- ts/src/lib/generators/utils.ts
+++ ts/src/lib/generators/utils.ts
@@ -3,8 +3,15 @@ import type { Expression } from '@babel/types'
 
 import { generate } from '../generate.js'
 
+export const producesReference = (expression: Expression) : boolean => {
+  return [
+    'Identifier',
+    'MemberExpression',
+  ].includes(expression.type)
+}
+
 export const GetValue = (expression: Expression) : string => {
-  if (expression.type === 'Identifier' || expression.type == 'MemberExpression') {
+  if (producesReference(expression)) {
     return `GetValue(${generate(expression)})`
   } else {
     return generate(expression)
