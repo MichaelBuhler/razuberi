bd5238b3212691373bea769d71fd32554f16682e
Michael Buhler <michael@michaelbuhler.com>
2023-07-13 00:05:43 +0700

    stringify string literals a little better

--- ts/src/lib/generators/StringLiteral.ts
+++ ts/src/lib/generators/StringLiteral.ts
@@ -4,5 +4,5 @@ import type { StringLiteral } from '@babel/types'
 import type { Generator } from './types.js'
 
 export const StringLiteralGenerator: Generator<StringLiteral> = ({ value }) => {
-  return `make_shared<String>("${value}")`
+  return `make_shared<String>(${JSON.stringify(value)})`
 }
